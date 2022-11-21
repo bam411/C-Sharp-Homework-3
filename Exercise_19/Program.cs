8937df65c6c8fedc5c177d4e0e13a132a3cfb4c4
Petrushov Denis <bam411@yandex.ru>
2022-11-21 15:51:33 +0300

    exercise 19, 21, 23

---
+++ Exercise_19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int number = ReadInt("Введите пятизначное число: ");
+string numText = Convert.ToString(number);
+
+if (numText.Length == 5)
+{
+    if (numText[0] == numText[4] || numText[1] == numText[3])
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Ведите правильное число");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Exercise_21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int aX = ReadInt("Введите координату X точки A: ");
+int aY = ReadInt("Введите координату Y точки A: ");
+int aZ = ReadInt("Введите координату Z точки A: ");
+int bX = ReadInt("Введите координату X точки B: ");
+int bY = ReadInt("Введите координату Y точки B: ");
+int bZ = ReadInt("Введите координату Z точки B: ");
+
+int sqrSideX = (bX - aX) * (bX - aX);
+int sqrSideY = (bY - aY) * (bY - aY);
+int sqrSideZ = (bZ - aZ) * (bZ - aZ);
+double distance = Math.Sqrt(sqrSideX + sqrSideY + sqrSideZ);
+Console.WriteLine(distance);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
